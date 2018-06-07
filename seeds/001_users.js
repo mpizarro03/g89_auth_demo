51542aa1b444aa59d7c986f69eaad18a00537d2e
Melissa Pizarro <mpizarro03@gmail.com>
2018-06-07 14:33:01 -0600

    seed works

--- seeds/001_users.js
+++ seeds/001_users.js
@@ -1,13 +1,11 @@
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+exports.seed = function (knex, Promise) {
+  return knex('users').del()
+    .then(() => (
+      knex('users').insert([{
+        id: 1,
+        username: 'hello@me.com',
+        password: '$2a$10$REbk3qsthnNR8XGTeir1J.jdVOva3aURv3Um4MVqa6L8C2G3eHpg2'
+      }])
+    ))
+    .then(() => knex.raw(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));`))
+}

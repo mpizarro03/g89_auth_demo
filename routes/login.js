21cbc38a00b585a9f31b0f58f018c2eaafab0acb
Melissa Pizarro <mpizarro03@gmail.com>
2018-06-07 14:51:31 -0600

    min password username checking

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,36 @@
 const express = require('express')
 const router = express.Router()
+const knex = require('../knex')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 router.get('/', function(req, res, next) {
   res.render('login', { errorMessage: '' })
 })
+
+router.post('/', function (req, res, next) {
+  const username = req.body.username
+  const password = req.body.password
+
+  if (username && password) {
+    knex('users')
+      .where('username', username)
+      .then((result) => {
+        if (result.length !== 1) {
+          res.status(400).send(JSON.stringify({ message: 'Bad username'}))
+        }
+        else if (bcrypt.compareSync(password, result[0].password)) {
+          res.status(200).send(JSON.stringify({ message: 'You HACK THE GIBSON'}))
+        }
+        else {
+          res.status(400).send(JSON.stringify({ message: 'Bad password'}))
+        }
+      })
+  }
+  else {
+    res.status(400).send(JSON.stringify({ message: 'Must username and password'}))
+  }
+})
+
 module.exports = router
